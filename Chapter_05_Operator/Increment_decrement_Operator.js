913b22a3d3f66aaa1f580a26d370b874d3548f12
Maruf <marufsherikar32@gmail.com>
2026-08-05 17:57:37 +0530

    Increment and decrement operator

---
+++ Chapter_05_Operator/Increment_decrement_Operator.js
@@ -0,0 +1,27 @@
+// Pre Inmcrement
+// let a = 10;
+// a = a + 1;
+// console.log(a); // 11
+
+
+// let  b = 10;
+// let c = ++b;
+// console.log(c);
+
+
+
+//Post Increment
+// let a = 10;
+// let b = a++;
+// console.log(b);
+
+
+// let a = 10;
+// let b = --a;
+// console.log(b);
+// console.log(a);
+
+let a = 10;
+let b = a--;
+console.log(b);
+console.log(a);
