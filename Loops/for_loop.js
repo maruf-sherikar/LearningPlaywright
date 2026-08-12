97fbb0b124f4cc9967d7a8e31e88bb76107c13f0
Maruf <marufsherikar32@gmail.com>
2026-08-12 17:56:03 +0530

    Loops

---
+++ Loops/for_loop.js
@@ -0,0 +1,17 @@
+let a = 10;
+
+for(let i = 0; i <a; i++){
+    console.log(i);
+}
+
+
+// for(let i = 0; i <a; ++i){
+//     console.log(i);
+// }
+
+console.log("===============");
+
+let maruf = 7
+for(let maruf = 0; maruf<7; maruf++){
+    console.log(maruf)
+}
---
+++ Loops/while_loop.js
@@ -0,0 +1,21 @@
+let attempts= 1;
+
+while(attempts <= 3){
+    console.log("attempt", attempts);
+    attempts++;
+}
+
+   
+
+
+
+let age = 7;
+
+while(true){
+    if(age>10){
+        break;
+    }
+    console.log(age);
+    age++;
+}
+
